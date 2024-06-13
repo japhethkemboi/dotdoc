4d7bf7c3c2b2dd9fc14d0fddbc7831cc9af34863
Japheth Kemboi <114729964+TheeAnon@users.noreply.github.com>
2024-06-13 16:52:14 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
+        I name="(tabs)" />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
